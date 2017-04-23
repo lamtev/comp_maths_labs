e4fbb0c11be5c9d5769f2a598e64732d7979f544
Anton Lamtev <antonlamtev@gmail.com>
2017-04-23 19:29:37 +0300

    add deploy

--- coursework/build.sh
+++ coursework/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+docker run -v $TRAVIS_BUILD_DIR/${LAB}:/${LAB}  lamtev/python:latest /bin/bash -c " cd ${LAB}/notebook && ipython nbconvert --to html car.ipynb "
 docker run -v $TRAVIS_BUILD_DIR/${LAB}:/${LAB}  lamtev/latex:full /bin/bash -c " cd ${LAB}/report/latex && pdflatex ${LAB}.tex && pdflatex ${LAB}.tex "
